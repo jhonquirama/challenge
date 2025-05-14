9538fa6a3df8c2d7ee0b217914f7ca487ba85538
Jhon Quirama <quirama80@gmail.com>
2025-05-14 07:47:03 -0500

    add test and linters

--- src/shared/utils/logger.ts
+++ src/shared/utils/logger.ts
@@ -1,15 +1,28 @@
 import winston from 'winston';
-import { serverConfig } from '../../infrastructure/config/server.config';
 
-const logger = winston.createLogger({
-  level: serverConfig.logLevel,
-  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+// Configuración del logger
+export const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
   defaultMeta: { service: 'notification-events-api' },
   transports: [
+    // Escribir logs a la consola
     new winston.transports.Console({
-      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
     }),
-  ],
+    // Escribir logs a un archivo
+    new winston.transports.File({ 
+      filename: 'logs/error.log', 
+      level: 'error' 
+    }),
+    new winston.transports.File({ 
+      filename: 'logs/combined.log' 
+    })
+  ]
 });
-
-export default logger;
